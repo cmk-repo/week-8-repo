68d2ef2a6caac50aaf822451dc2fed4a78ca7529
Chandan <tochandan@icloud.com>
2023-10-28 18:44:09 -0400

    custom-hook-may-work

--- client/src/Components/TodoList.tsx
+++ client/src/Components/TodoList.tsx
@@ -1,6 +1,7 @@
 import React, { useContext, useState, useEffect } from 'react';
 import { authState } from '../store/authState.js';
-import {useRecoilValue} from "recoil";
+import { useRecoilValue } from "recoil";
+import { Navigate } from 'react-router-dom';
 
 interface Todo {
     _id: string;
@@ -9,25 +10,54 @@ interface Todo {
     done: boolean;
 }
 
-type TodoArray = Todo[];
+//type TodoArray = Todo[];
 
-const TodoList = () => {
-    const [todos, setTodos] = useState<TodoArray>([]);
-    const [title, setTitle] = useState('');
-    const [description, setDescription] = useState('');
-    const authStateValue = useRecoilValue(authState);
 
+// adding custom hook
+
+function useTodos() {// should start with use for custom hook
+    //const [todos, setTodos] = useState<Todo[]>([]);
+    const [todos, setTodos] = useState([]);
+    const [loading, setLoading] = useState(true);
     useEffect(() => {
         const getTodos = async () => {
             const response = await fetch('http://localhost:3000/todo/todos', {
                 headers: { Authorization: `Bearer ${localStorage.getItem("token")}` }
             });
             // Todo: Create a type for the response that you get back from the server
-            const data: Todo[] = await response.json();
+            const data = await response.json();
             setTodos(data);
+            setLoading(false);
         };
         getTodos();
     }, []);
+
+    return {
+        loading,
+        todos: todos
+    }
+    //
+}
+
+const TodoList = () => {
+    //const [todos, setTodos] = useState<TodoArray>([]); // did custom hook
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const authStateValue = useRecoilValue(authState);
+    // custom hook call
+    const { loading, todos } = useTodos();
+
+    // useEffect(() => {
+    //     const getTodos = async () => {
+    //         const response = await fetch('http://localhost:3000/todo/todos', {
+    //             headers: { Authorization: `Bearer ${localStorage.getItem("token")}` }
+    //         });
+    //         // Todo: Create a type for the response that you get back from the server
+    //         const data: Todo[] = await response.json();
+    //         setTodos(data);
+    //     };
+    //     getTodos();
+    // }, []);
 
     const addTodo = async () => {
         const response = await fetch('http://localhost:3000/todo/todos', {
@@ -38,10 +68,10 @@ const TodoList = () => {
         const data = await response.json();
 
         let newTodos = [];
-        for (let i = 0; i<todos.length; i++) {
+        for (let i = 0; i < todos.length; i++) {
             newTodos.push(todos[i]);
         }
-        
+
         newTodos.push(data);
         setTodos(newTodos);
     };
@@ -57,12 +87,13 @@ const TodoList = () => {
 
     return (
         <div>
-            <div style={{display: "flex"}}>
+            <div style={{ display: "flex" }}>
                 <h2>Welcome {authStateValue.username}</h2>
-                <div style={{marginTop: 25, marginLeft: 20}}>
+                <div style={{ marginTop: 25, marginLeft: 20 }}>
                     <button onClick={() => {
                         localStorage.removeItem("token");
                         window.location = "/login";
+
                     }}>Logout</button>
                 </div>
             </div>
